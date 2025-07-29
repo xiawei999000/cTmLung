2b2bc6150228b415097dc6cbbde0cef5443d1e01
Wei Xia <33140203+xiawei999000@users.noreply.github.com>
2025-07-29 20:53:50 +0800

    Update 1_DFS_prediction-performance.R

--- 1_DFS_prediction-performance.R
+++ 1_DFS_prediction-performance.R
@@ -8,16 +8,11 @@ for (t_ggo in t_ggo_list) {
     feas_file_name <- paste(t_ggo, t_soild, sep = "")
     
     feas_feas_file_dir <- paste('D:/train_features/', feas_file_name, '.xlsx', sep = "")
-    info_feas_SYSUCC_train <- read.xlsx(feas_feas_file_dir, 1, header=TRUE) 
-    
-    # exclude the patient withsolid part diameter of node > 40 mm
-    info_feas_SYSUCC_train <- info_feas_SYSUCC_train[-which(info_feas_SYSUCC_train$Imaging_ID %in% exclude_id_SYSUCC),]
-    
-    x_train <- info_feas_SYSUCC_train[, 13:31]
-    
+    info_feas_train <- read.xlsx(feas_feas_file_dir, 1, header=TRUE)    
+    x_train <- info_feas_train[, 13:31]    
     # LASSO cox modelling 
-    DFS_time_train <- as.numeric(as.character(info_feas_SYSUCC_train$DFS))
-    DFS_event_train <- info_feas_SYSUCC_train$DFS_status
+    DFS_time_train <- as.numeric(as.character(info_feas_train$DFS))
+    DFS_event_train <- info_feas_train$DFS_status
     DFS_y_train <- survival::Surv(DFS_time_train, DFS_event_train)
     set.seed(fix_seed)
     cvfit <- cv.glmnet(as.matrix(x_train),DFS_y_train,
